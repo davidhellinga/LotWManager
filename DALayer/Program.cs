ecf7190e19a2af36314abb68341690b6016a90b9
davidhellinga <davidhellinga@gmail.com>
2017-09-14 10:52:30 +0200

    Project initialisation

---
+++ DALayer/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DALayer
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+        }
+    }
+}
